54a0f2edebfaa4fdd314aac07c9ba3bb619481f7
jtkaczyszyn <jtkaczyszyn@elementum.com>
2018-05-24 12:45:39 -0300

    auth goes directly to google stragegy

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const SIGNIN_URL = 'https://us-central1-lsi7571.cloudfunctions.net/hello-world/auth/signin/';
+export const SIGNIN_URL = 'https://us-central1-lsi7571.cloudfunctions.net/hello-world/auth/google/';

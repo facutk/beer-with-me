61147fd7c5829664e77ae49929a9b61661bbc22e
facu.tk <facutk@MacBook-Pro-de-facutk.local>
2018-05-26 16:31:14 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "comma-dangle": ["error", "never"]
+    },
+    "globals": {
+        "fetch": false
+    },
+    "env": {
+        "browser": true
+    }
+};
